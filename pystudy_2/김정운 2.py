b3f9b007c8b36c53a9f917c0f95af671ba348672
jungun1111 <Rickcats!1>
2022-06-22 15:38:35 +0900

    update

---
+++ pystudy_2/김정운 2.py
@@ -0,0 +1,18 @@
+alist = [1, 2, 3, 4, 5]
+
+print(alist)
+
+if 4 in alist:
+    print("1이 있다")
+
+if 14 not in alist:
+    print("14가 없당")
+
+
+print(alist[4])
+
+
+a = [3,5,6,9]
+b = [10,11,12]
+print(a+b)
+print(len(a))
