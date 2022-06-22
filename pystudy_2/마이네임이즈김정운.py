b9ed20f57e7216e7b8538a7d91ef08bb6cd68011
jungun1111 <Rickcats!1>
2022-06-22 15:05:01 +0900

    update

---
+++ pystudy_2/마이네임이즈김정운.py
@@ -0,0 +1,17 @@
+score = float(input("> 나의 인생 점수: "))
+
+if score == 10:
+    print("매우 굿굿")
+elif 9 <= score:
+    print("좀 하네")
+elif 8 <= score:
+    print("평범")
+elif 7 <= score:
+    print("노력해라")
+elif 6 <= score:
+    print("죽기 살기로 노력해라")
+else:
+    print("소생불가")
+
+
+
